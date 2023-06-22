7d762311b84256729d90d5f9a85f946c389ef3b7
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-06-22 17:37:07 -0400

    add route handlers & function & middleware Enabling extended URL-encoded body parsing middleware add app.get & app.post - GET requests to "/urls/new" now generate a webpage based on the "urls_new" template. - POST requests to "/urls" log the request body to the console and respond with "Ok".
    add generateRandomString function which generates a random 6 alphanumeric key for later use in urlDatabase keys

--- express_server.js
+++ express_server.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs')
+
+function generateRandomString() {
+  const alphanumericChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * alphanumericChars.length);
+    randomString += alphanumericChars.charAt(randomIndex);
+  }
+
+  return randomString;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
@@ -33,4 +50,8 @@ app.get("/hello", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
