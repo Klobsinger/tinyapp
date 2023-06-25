17c9f867a108368cdc0a41e4cde48f35d87c86a9
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-06-25 12:05:46 -0400

    add cookieParsing added cookieParsing with cookieParser sending username cookie data to ejs templates using templateVars

--- express_server.js
+++ express_server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser())
 app.set('view engine', 'ejs')
 
 function generateRandomString() {
@@ -23,11 +24,14 @@ const urlDatabase = {
 };
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  templateVars = { username: req.cookies['username'] }
+  res.render("urls_new",templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    username: req.cookies['username'],
+    id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
@@ -36,7 +40,9 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies['username'],
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
